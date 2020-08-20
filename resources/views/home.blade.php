f61b76a9a8c21da3e1bf3cd41f653f1e3e21ce5b
AhmadSuherman <suhermana274@gmail.com>
2020-08-20 21:59:45 +0700

    [W] Modified Fitur Home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,80 @@
-@extends('layouts.app')
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Bootstrap Example</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+</head>
+<body>
 
-@section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
-                </div>
-            </div>
-        </div>
+<ul class="nav nav-tabs">
+  <li class="nav-item">
+    <a class="nav-link active" href="#">Home</a>
+  </li>
+  <li class="nav-item dropdown">
+    <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#">Progres</a>
+    <div class="dropdown-menu">
+      <a class="dropdown-item" href="#">Progres 1</a>
+      <a class="dropdown-item" href="#">Progres 2</a>
+      <a class="dropdown-item" href="#">Progres 3</a>
     </div>
+  </li>
+    <li class="nav-item">
+    <a class="nav-link disabled" href="#">About</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link disabled" href="#">Contact</a>
+  </li>
+</ul>
+
+
+<div class="container">
+  <h2>My Team Cuyang</h2>
+  <p></p>            
+  <table class="table table-hover">
+    <thead>
+      <tr>
+        <th>Firstname</th>
+        <th>Lastname</th>
+        <th>Email</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td>Ahmad</td>
+        <td>Suherman</td>
+        <td>suherman@gmail.com.com</td>
+      </tr>
+      <tr>
+        <td>Candra</td>
+        <td>Aja</td>
+        <td>candra@gmail.com</td>
+      </tr>
+      <tr>
+        <td>Dede</td>
+        <td>Suminar</td>
+        <td>Desum@example.com</td>
+      </tr>
+      <tr>
+        <td>Hania</td>
+        <td>Firianti</td>
+        <td>hania@example.com</td>
+      </tr>
+      <tr>
+        <td>Santi</td>
+        <td>Sintiawati</td>
+        <td>santuy2003@example.com</td>
+      </tr>
+
+
+    </tbody>
+  </table>
 </div>
-@endsection
+
+</body>
+</html>
+
