db9d45abe08e7c4a2422ac12d15460479e00cfc6
AhmadSuherman <suhermana274@gmail.com>
2020-09-17 22:22:27 +0700

    [W][NEW] Membuat Page Error dan Kerangka View

--- resources/views/kerangka_view.blade.php
+++ resources/views/kerangka_view.blade.php
@@ -3,15 +3,15 @@
 @section('content')
 
 <div class="main-content">
-      <div class="row">
-          <div class="col-12">
-              <div class="card">
-                  <div class="card-body">
-                      
-                  </div>
-              </div>
-          </div>
-      </div>
+    <div class="row">
+        <div class="col-12">
+            <div class="card">
+                <div class="card-body">
+
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
-    
+
 @endsection
